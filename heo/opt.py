correcting factor of sigma (no effect on results)

heo/opt.py
@@ -11,7 +11,7 @@ class heo():
         self.opt_config = opt_config
 
     def phi(self, mu, sigma):
-        return torch.erf(mu / sigma / np.sqrt(2))
+        return torch.erf(mu / sigma)
     
     def initialize(self):
         self.theta = torch.ones(self.dim, device=self.device) * 0.5
@@ -20,7 +20,7 @@ class heo():
         self.energy = self.energy_func(x)
     
     def update(self, t, T):
-        sigma = (1 - t / T)
+        sigma = (1 - t / T) * np.sqrt(2)
         x = self.phi(self.theta - torch.rand(self.dim, device=self.device), sigma)
         self.energy = self.energy_func(x)
         self.energy.backward()

mvc.py
@@ -27,7 +27,7 @@ def refine(x):
     return x
 
 def phi(mu, sigma):
-    return torch.erf(mu / sigma / np.sqrt(2))
+    return torch.erf(mu / sigma)
 
 def heo_mvc(dim, enegry_func, config):
     p = torch.ones(dim, device='cuda:0') * 0.5
@@ -38,7 +38,7 @@ def heo_mvc(dim, enegry_func, config):
         opt=optim.Adam([p], lr=config['lr'])
     for t in range(config['T']):
         C = t / config['T'] * config['C_factor']
-        sigma = (1 - t / config['T'])
+        sigma = (1 - t / config['T']) * np.sqrt(2)
         x = phi(p - torch.rand(dim, device='cuda:0'), sigma)
         energy = enegry_func((x + 1) / 2, C)
         energy.backward()

tenaray_nn.py
@@ -13,7 +13,7 @@ os.makedirs(workdir,exist_ok=True)
 logger = Logger(f'{workdir}/log.log','nn')
 
 def phi(mu, sigma):
-    return torch.erf(mu / sigma / np.sqrt(2))
+    return torch.erf(mu / sigma)
 
 def heo_train(dim, config, rep_energy=False):
     p1 = torch.rand([dim2, dim], device='cuda:0')
@@ -27,7 +27,7 @@ def heo_train(dim, config, rep_energy=False):
         energy_s = []
         uncern_s = []
     for t in range(config['T']):
-        sigma = (1 - t / config['T'])
+        sigma = (1 - t / config['T']) * np.sqrt(2)
         W = (phi(p1 - torch.rand([dim2, dim], device='cuda:0'), sigma) +
              phi(p2 - torch.rand([dim2, dim], device='cuda:0'), sigma)) / 2
         energy = ((new_work_ensemble(W, x) - y_real) ** 2).mean()

variable_selection.py
@@ -12,7 +12,7 @@ import os
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 def phi(mu, sigma):
-    return torch.erf(mu / sigma / np.sqrt(2))
+    return torch.erf(mu / sigma)
 
 
 def heo_linear_reg(dim, x_s, y_s, config):
@@ -25,7 +25,7 @@ def heo_linear_reg(dim, x_s, y_s, config):
     opt2 = optim.SGD([beta],lr=config['lr']/ratio_lr,momentum=config['momentum'])
 
     for t in range(config['steps']):
-        sigma = (1 - t / config['steps'])
+        sigma = (1 - t / config['steps']) * np.sqrt(2)
         x = (phi(theta - torch.rand(dim, device='cuda:0'), sigma) + 1) / 2
         pred_y = ((x * beta).reshape(-1, 1) * x_s).sum(dim=0)
         energy = ((y_s - pred_y) ** 2).mean()